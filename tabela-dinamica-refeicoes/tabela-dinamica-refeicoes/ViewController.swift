da794876118381e6502e8fc2ed03c7190eba2858
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-21 00:27:10 -0300

    Criado tableViewController dinamico

--- tabela-dinamica-refeicoes/tabela-dinamica-refeicoes/ViewController.swift
+++ tabela-dinamica-refeicoes/tabela-dinamica-refeicoes/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
+    let refeicoes = ["Churros", "Macarrão", "Pizza"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("tableviewcontroller foi carregado")
+        print("TableViewController foi carregado")
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celula = UITableViewCell( style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao
+        
+        return celula
+    }
 }
-
