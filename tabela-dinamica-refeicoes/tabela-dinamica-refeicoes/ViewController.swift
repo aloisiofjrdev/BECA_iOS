dcfabe947c043b3d660793fbb7353e2062d6ab70
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-21 00:01:24 -0300

    Criado projeto tabela-dinamica-refeicoes

---
+++ tabela-dinamica-refeicoes/tabela-dinamica-refeicoes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabela-dinamica-refeicoes
+//
+//  Created by Aloisio Formento Junior on 20/12/20.
+//  Copyright © 2020 Aloisio Formento Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("tableviewcontroller foi carregado")
+    }
+
+
+}
+
