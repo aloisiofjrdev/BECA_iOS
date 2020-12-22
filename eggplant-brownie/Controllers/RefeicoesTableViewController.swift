e2df66cd19965dd8a0adc22b31ae1fe08a0c913c
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-22 09:41:27 -0300

    Salvando itens

--- eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -15,8 +15,7 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
                      Refeicao(nome: "Comida Japonesa", felicidade: 5)]
     
     override func viewDidLoad() {
-        guard let diretorio = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else{return}
-        let caminho = diretorio.appendingPathComponent("refeicao")
+        guard let caminho = recuperaCaminho()else{return}
         do {
             let dados = try Data(contentsOf: caminho)
             guard let refeicoesSalvas = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(dados) as? Array<Refeicao> else {return}
@@ -25,6 +24,13 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
             print(error.localizedDescription)
         }
     }
+    
+    func recuperaCaminho () -> URL? {
+        guard let diretorio = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else{return nil}
+        let caminho = diretorio.appendingPathComponent("refeicao")
+        return caminho
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return refeicoes.count
@@ -46,8 +52,7 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
         refeicoes.append(refeicao)
         tableView.reloadData()
         
-        guard let diretorio = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else{return}
-        let caminho = diretorio.appendingPathComponent("refeicao")
+        guard let caminho = recuperaCaminho() else{return}
         
         do {
             let dados = try NSKeyedArchiver.archivedData(withRootObject: refeicoes, requiringSecureCoding: false)
